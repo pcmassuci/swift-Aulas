f5df5b6da98db737633415d414fb8021698d8357
Paulo Massuci <pcmassuci@gmail.com>
2017-12-06 10:28:29 -0200

    meus projetos

---
+++ Google+/SignGooglePoc/SignGooglePoc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SignGooglePoc
+//
+//  Created by Paulo César Morandi Massuci on 06/12/2017.
+//  Copyright © 2017 Paulo César Morandi Massuci. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
